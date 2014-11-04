376eabc888cf5a4684ae604e1c2306632e87167d
Andreas Lindhé <Lindhe94@gmail.com>
2014-11-04 22:50:49 +0100

    Remove footer, fix hash-navigation and minor fixes
    Footer is hidden until it's finnished, a javascript is now monitoring
    the url hash (in a very suboptimal way) and most things are done so far
    that it might be a possibility to launch the site.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function () {
+	if (window.location.hash == "#IT" || window.location.hash == "#it") {
+		$('#myTab a#ittab').tab('show');
+	} else if (window.location.hash == "#Data-VS-IT" || window.location.hash == "#data-vs-it") {
+		$('#myTab a#vstab').tab('show')
+	}
+});
+
+
 $("#ittab").click(function(){
     $("#nav-header").addClass("it").removeClass("data vs");
 });
